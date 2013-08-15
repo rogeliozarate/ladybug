fb74b987a7682264a46b6b05e184a123455ce557
Rogelio Zarate <i.am@rogeliozarate.com>
2013-08-14 19:51:58 -0500

    Add MongoDB support to app
    - Add mongoid gem
    - Configure mongoid for development
      Note that the config file mongoid.yml must be strict YAML, this is complete with indents
    - Create class Page, inheriting from Mongoid Document class

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ ruby "2.0.0"
 
 gem "sinatra"
 gem "sinatra-reloader"
+gem "haml"
+gem "sass"
+gem "mongoid"
+gem "redcarpet"
--- app.rb
+++ app.rb
@@ -1,7 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require 'mongoid'
+require 'haml'
+require 'sass'
+require 'redcarpet'
 
+configure :development do
+  enable :logging, :dump_errors, :run, :sessions
+  Mongoid.load!("config/mongoid.yml")
+end
 
+class Page
+  include Mongoid::Document
+  field :title,       type: String
+  field :content,     type: String
+end
 
 get '/' do
   "home"
